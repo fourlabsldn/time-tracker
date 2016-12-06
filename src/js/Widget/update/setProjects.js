0a664e7dca65d2dde5a5ad17f4c912e84a12efba
lazamar <lazaronijunior@gmail.com>
2016-12-06 10:14:29 +0000

    Implement setProjects

---
+++ src/js/Widget/update/setProjects.js
@@ -0,0 +1,13 @@
+import { isRecording, updateAt } from './utils';
+import { pipe } from 'ramda';
+import assert from 'fl-assert';
+
+export default (model, action) => {
+  assert(!isRecording(model), 'Trying to update project array whilst recording');
+
+  const newProjects = action.rawProjects;
+  return pipe(
+    updateAt('unselectedProjects', newProjects),
+    updateAt('selectedProject', null),
+  )(model);
+};
