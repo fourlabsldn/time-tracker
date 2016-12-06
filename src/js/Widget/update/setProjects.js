61a4b7b49b79c81fd1f941922a8e2f46650362bc
lazamar <lazaronijunior@gmail.com>
2016-12-06 11:35:52 +0000

    Preprocess projects

--- src/js/Widget/update/setProjects.js
+++ src/js/Widget/update/setProjects.js
@@ -1,11 +1,28 @@
 import { isRecording, updateAt } from './utils';
-import { pipe } from 'ramda';
+import { pipe, map } from 'ramda';
 import assert from 'fl-assert';
+
+const processRawDeliverables = map(
+    ({ name, url }) => ({
+      name,
+      url,
+      recording: null,
+    })
+);
+
+const processRawProjects = map(
+  ({ name, url, deliverables }) => ({
+    name,
+    url,
+    unselectedDeliverables: processRawDeliverables(deliverables),
+    selectedDeliverable: null,
+  })
+);
 
 export default (model, action) => {
   assert(!isRecording(model), 'Trying to update project array whilst recording');
 
-  const newProjects = action.rawProjects;
+  const newProjects = processRawProjects(action.rawProjects);
   return pipe(
     updateAt(['unselectedProjects'], newProjects),
     updateAt(['selectedProject'], null),
